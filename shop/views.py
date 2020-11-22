5a18f8b008f6253e289541b8628e7709078d7f70
dante <thornhillfox@gmail.com>
2020-11-22 13:23:38 +0300

    cart add

--- shop/views.py
+++ shop/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from django.http import HttpResponse
+from cart.forms import CartAddProductForm
 
 
-
-
-# Create your views here.
-# def product_list(request, category_slug=None):
-# 	category = None
-# 	categories = Category.objects.all()
-# 	products = Product.objects.filter(available=True)
-# 	if category_slug:
-# 		category = get_object_or_404(Category, slug = category_slug)
-# 		products = products.filter(category=category)
-# 		return render(request, 'shop/product/list.html',
-# 			{
-# 			'category': category,
-# 			'categories': categories,
-# 			'products': products
-# 			})
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -34,4 +19,7 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
 	product = get_object_or_404(Product, id=id, slug=slug, available=True)
-	return render(request, 'shop/product/detail.html', { 'product': product })
+	cart_product_form = CartAddProductForm()
+	return render(request, 'shop/product/detail.html', { 'product': product,
+														'cart_product_form': cart_product_form
+	 })
